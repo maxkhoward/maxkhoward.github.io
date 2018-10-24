9a5674b7d856842a8b79c199914fbd5007b856ab
Mathias Bynens <mathias@mailinator.com>
2018-10-24 16:54:25 -0700

    Add gulp to project

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+
+// gulp.task('log', function() {
+//   gutil.log('== My Log Task ==')
+// });
+//
+// gulp.task('copy', function() {
+//   gulp.src('index.html')
+//   .pipe(gulp.dest('assets'))
+// });
+
+gulp.task('sass', function() {
+  gulp.src('src/scss/index.scss')
+  .pipe(sass({style: 'minified'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('css/'))
+});
+
+gulp.task('copy', function() {
+  gulp.src('build/**/*.*')
+  .pipe(gulp.dest('/'))
+});
